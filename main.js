40617a4710c716d5f1f99b3809d865c5d1688f79
Md Ismail <112852261+mdaleardc@users.noreply.github.com>
2024-04-09 14:29:44 +0600

    My portfolio web page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+	strings: ['Frontend Developer!', 'Responsive Web Developer!', 'Web Developer!'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 2000,
+	loop: true
+});

d95e0b6b644711fa4514163a5c1ac39eccfdaf4d
Md Ismail <112852261+mdaleardc@users.noreply.github.com>
2024-04-10 12:35:05 +0600

    To update files
    have been update all of 3 files

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ var typed = new Typed('.text', {
 	backDelay: 2000,
 	loop: true
 });
+
+const date = new Date();
+const day = date.getDate() + 1;
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+const dateArr = [day, month, year].join("-");
+console.log(dateArr);
+
+
+if (dateArr === "11-4-2024" || dateArr === "12-4-2024" || dateArr === "13-4-2024") {
+    const promptBox = prompt("Enter Your Name", "");
+    alert(`Assalamu Alikum ${promptBox}, Happy Eid Mumarak!!!`);
+};
+
+
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesdy", "Wednesday", "Friday", "Saturday"];
+const dayIndex = date.getDay();
+const dayName = daysOfTheWeek[dayIndex];
+
+if (dayName === "Friday") {
+    alert("Happy Jumaah Mubarak!");
+}; 
+
+
